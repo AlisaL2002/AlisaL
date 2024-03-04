0f64d611a47c9e11516bf222e955227124451929
Alisa Logacheva <logacheva.alisa@gmail.com>
2024-03-04 18:37:22 -0500

    Implemented a new Yoga exercise page with buttons & created a YogaExercise component

--- lab6/src/components/DurationExercise.js
+++
@@ -1,9 +0,0 @@
-import StopWatch from './stopWatch';
-export function DurationExercise({exercise, setMenuScreen}) {
-    let {name} = exercise
-    return <div>
-      <p>{name}</p>
-    <StopWatch/>
-    <button onClick={setMenuScreen}>Back to Menu</button>
-    </div>
-  } 
